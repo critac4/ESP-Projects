a3c03030e2b28524439755aea34f0edea46bb6d0
Critac <critac4@gmail.com>
2025-07-28 17:56:27 +0530

    added more comments, left the commented artifacts of the previous implementations

--- blinkman/main/main.c
+++ blinkman/main/main.c
@@ -7,35 +7,39 @@
 #define GPIO_OUT_W1TC_REG ((volatile uint32_t *)0x3FF4400c)
 #define GPIO_ENABLE_W1TS_REG ((volatile uint32_t *)0x3FF44024)
 
-//set IO MUX GPIO #1 as GPIO1 pin directly from IO_MUX by changing the function of the
-// pin from "function 0" {U0TXD} to "function 2" {GPIO1}
 
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+//  THE SERIAL WILL NOT WORK SINCE THE U0TXD IS RECONFIGURED TO THE GPIO1 PIN
+//  AND SINCE ONLY ONE PIN CONNECTS TO THE CP2102 (THE U0TXD PIN.. ) AS LONG AS ITS 
+//  CONFIGURED AS GPIO1, THE SERIAL WILL NOT WORK.
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 void led_on()
 {
     // 2. Drive GPIO1 HIGH
-    *GPIO_OUT_W1TS_REG = (1 << 1);  // GPIO_OUT_W1TS_REG
-    printf("LED ON\n");
+    *GPIO_OUT_W1TS_REG = (1 << 1); // GPIO_OUT_W1TS_REG
+    //printf("LED ON\n");
 }
 
 void led_off()
 {
     // 2. Drive GPIO1 HIGH
-    *GPIO_OUT_W1TC_REG = (1 << 1);  // GPIO_OUT_W1TC_REG
-    printf("LED OFF\n");
+    *GPIO_OUT_W1TC_REG = (1 << 1); // GPIO_OUT_W1TC_REG
+    //printf("LED OFF\n");
 }
 
 void app_main(void)
 {
-    //set IO MUX GPIO #1 as GPIO1 pin directly from IO_MUX by changing the function of the
-    // pin from "function 0" {U0TXD} to "function 2" {GPIO1}
-    // This is done by setting the 13th bit of IO_MUX_U0TXD_REG to 1
-    *IO_MUX_U0TXD_REG = (1<<13);
+    // set IO MUX GPIO #1 as GPIO1 pin directly from IO_MUX by changing the function of the
+    //  pin from "function 0" {U0TXD} to "function 2" {GPIO1}
+    //  This is done by setting the 13th bit of IO_MUX_U0TXD_REG to 1
+    *IO_MUX_U0TXD_REG = (1 << 13);
 
     // 1. Enable output driver for GPIO1
-    *GPIO_ENABLE_W1TS_REG = (1 << 1);  // GPIO_ENABLE_W1TS_REG
+    *GPIO_ENABLE_W1TS_REG = (1 << 1); // GPIO_ENABLE_W1TS_REG
 
     // // 2. Drive GPIO2 HIGH
     // *((volatile uint32_t *)0x3FF44008) = (1 << 1);  // GPIO_OUT_W1TS_REG
@@ -44,15 +48,17 @@ void app_main(void)
     // *((volatile uint32_t *)0x3FF4400c) = (1 << 1);  // GPIO_OUT_W1TC_REG
 
     // 3. Prevent task watchdog panic by delaying
-    while (1) {
-        
+    while (1)
+    {
+
         led_on();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Sleep for 1 second
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Sleep for 1 second
 
         led_off();
-        vTaskDelay(3000 / portTICK_PERIOD_MS);  // Sleep for 1 second
-        
-        printf("Hello, world!\n");
+        vTaskDelay(3000 / portTICK_PERIOD_MS); // Sleep for 1 second
+
+        //printf("Hello, world!\n");
         // vTaskDelay(1000 / portTICK_PERIOD_MS);  // Sleep for 1 second
+
     }
 }
